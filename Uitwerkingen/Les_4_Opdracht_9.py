a76f897ec2aa6cb0dc5b4afdc1c1ff106d8a82c1
T.J.E. den Hartog <99054715@mydavinci.nl>
2023-09-04 19:20:28 +0200

    Opdracht 9 afgemaakt

---
+++ Uitwerkingen/Les_4_Opdracht_9.py
@@ -0,0 +1,22 @@
+# Rekensom A
+#print("Som A")
+#print(23 * 1357)
+
+# Rekensom B
+#print("Som B")
+#print(3.4 + 2.45 + 1.95 / 100 * 0.9)
+
+
+
+getal1 = 23
+getal2 = 1357
+getal3 = 3.4
+getal4 = 2.45
+getal5 = 1.95
+getal6 = 9
+getal7 = 109
+
+print("Som 1")
+print(getal1 * getal2)
+print("Som 2")
+print((getal3 + getal4 + getal5) / getal7 * getal6 )
