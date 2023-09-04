2087515e818eb6bfb6c126f441cef8671bd04649
T.J.E. den Hartog <99054715@mydavinci.nl>
2023-09-04 18:02:48 +0200

    Opdracht gemaakt

---
+++ Uitwerkingen/Les_4_Opdracht_7.py
@@ -0,0 +1,6 @@
+#Delen doet soms iets vreemds! Bekijk de volgende code
+print( (431 / 100) * 100 )
+#Wat denk je dat eruit komt? MIJN ANTWOORD: 431 En test het daarna eens!
+#Wat gebeurt hier? Schrijf dit op als commentaar!
+
+#COMMENTAAR: Python die heeft geen zin om het getal af te ronden.
