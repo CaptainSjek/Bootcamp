ab169482b28a8dc687e47f299524877f39331a44
CaptainSjek <99054715@mydavinci.nl>
2023-09-04 13:56:20 +0100

    Opdracht 2 opgelost + verklaring toegevoegd

---
+++ Uitwerkingen/Les_4_Opdracht_2.py
@@ -0,0 +1,8 @@
+print( 5*2 -3+4/2 )
+print( 5*2 - 3+4 / 2 )
+print("Antwoord op vraag: krijg je nu dezelfde uitkomst? = Ja")
+
+print( (5*2) - (3+4) /2 )
+print("(5*2) - (3+4) /2 verklaring: rekenvolgorde: 5x2 = 10    3+4= 7  7/ 2 = 3,5    10 - 3.5 = 6.5")
+print( ((5*2) - (3+4)) /2 )
+print("((5*2) -(3+4)) /2 verklaring: 5x2=10 3+4=7  10-7 = 3  3/2= 1.5 ")
