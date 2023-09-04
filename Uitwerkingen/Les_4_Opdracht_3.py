2fa0d21c04e2384d2a91cfbe61fead80eed1858a
CaptainSjek <99054715@mydavinci.nl>
2023-09-04 14:08:56 +0100

    Antwoord gegeven op vraag uit de opdracht

--- Uitwerkingen/Les_4_Opdracht_3.py
+++ Uitwerkingen/Les_4_Opdracht_3.py
@@ -1,3 +1,5 @@
 naam= ("tim")
 tekst = "Hallo " + naam +", ik leer nu programmeren."
 print(tekst)
+
+print("Vraag uit opdracht: Welke Operator(s) heb je gebruikt? Antwoord: ik heb '=' gebruikt en de '+' ")
