b58fcea929d1428a5892d58fe93e1be3275f7ef2
CaptainSjek <99054715@mydavinci.nl>
2023-09-04 13:42:31 +0100

    Sommen uit laten rekenen + verklaring toegevoegd

---
+++ Uitwerkingen/Demo.py
@@ -0,0 +1,7 @@
+#naam = "Tim"
+#naam = naam + " " "den" " " "Hartog"
+
+naam = input("Voer hier je naam in ")
+achternaam = input("Voer hier je achternaam in ")
+volledigeNaam = naam + " " + achternaam
+print(volledigeNaam)
---
+++ Uitwerkingen/les_3_opdracht_4.py
@@ -0,0 +1,12 @@
+woord_1 = input("Wat voor soort dag is het? ")
+stad_1 = input("Welke stad wil je in het verhaaltje? ")
+naam_1 = input("Welke naam wil je gebruiken? ")
+woord_3 = input("Wat voor voorwerp wil je in het verhaaltje? ")
+woord_4 = input("Noem een persoonlijkheid van iemand. ")
+woord_5 = input("Noem iets wat iemand kan verkopen. ")
+woord_6 = input("Noem een locatie/plek. ")
+woord_zin_7 = input("Noem een bezigheid dat een groepje mensen kan doen. ")
+
+
+
+print("Het was een " + woord_1 + " dag in " + stad_1 + ". " + naam_1 + " liep door de straten, op zoek naar een " + woord_3 + ". " + " Plotseling kwam " + naam_1 + " een " + woord_4 + " man/vrouw tegen die een " + woord_5 + " verkocht. " + naam_1 + " besloot om het te kopen en liep verder. Toen " + naam_1 + " bij een " + woord_6 + " kwam, zag hij/zij een groep mensen die " + woord_zin_7 + ". " +  naam_1 + " besloot om mee te doen en had de tijd van zijn/haar leven.")
