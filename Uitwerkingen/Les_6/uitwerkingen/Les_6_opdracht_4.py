9f49a142f89ff5de4dbc746db7a77c7d54dd4955
CaptainSjek <99054715@mydavinci.nl>
2023-09-05 13:11:37 +0100

    Update Les_6_opdracht_4.py

---
+++ Uitwerkingen/Les_6/uitwerkingen/Les_6_opdracht_4.py
@@ -0,0 +1,9 @@
+#Opdracht 4: 
+#Maak een lijstje van minimaal 6 verschillende vergelijkingen en geef van iedere vergelijking een voorbeeld.
+#Bijv.:
+#a = 3
+#b = 5 
+#print(a<b) 
+#print(a....
+#Schrijf er zo zelf nog eens minimaal 5 en test je code.
+
