dc52a1166869a911d2d641dd6ac9e2cc4578fa8d
CaptainSjek <99054715@mydavinci.nl>
2023-09-05 11:57:51 +0100

    Opgelost en toegeligd

---
+++ Uitwerkingen/Les_6/uitwerkingen/Les_6_opdracht_1.py
@@ -0,0 +1,14 @@
+#Wat klopt er niet in deze code:
+#	"if x = 18:
+#	    print('de waarde van x = 18')"
+
+#Wat klopt er niet in deze code:
+x=18
+if (x >= 18):
+    print("de waarde van  x = 18")
+
+    
+#Wat klopt er niet in deze code? 
+#Antwoord: If commando staat in een string
+#er is nog geen variabele ingesteld
+#print commando staat de string niet goed
