1d0a2bc85b5ff904e03a0c43c9d724d005e02207
CaptainSjek <99054715@mydavinci.nl>
2023-09-05 12:11:30 +0100

    Opdracht opgelost

---
+++ Uitwerkingen/Les_6/uitwerkingen/Les_6_opdracht_2.py
@@ -0,0 +1,20 @@
+#Schrijf een programma met twee variabelen: 
+# - a = 3
+# - b = 7
+#Bepaal met behulp van code welke variabele het hoogste getal is. 
+#Je laat je programma kiezen welke van de drie meldingen moeten worden gestuurd:
+#"Variabele a is het grootst want {waarde a} is groter dan {waarde b}"
+#"Variabele b is het grootst want {waarde b} is groter dan {waarde a}"
+#"Variabele a en b zijn gelijk."
+#Test je programma door a en b een paar keer een andere waarde te geven.
+#Als laatste stap laat je de gebruiker de getallen a en b invoeren. Zorg ervoor dat je programma goed blijft werken.
+
+a = 7
+b = 7
+
+if (a > b):
+    print("variabele a is het grootst want " , a , "is groter dan" , b)
+if (b > a):
+    print("variabele b is het grootst want" , b , "is groter dan" , a)
+if (a == b):
+    print("variabele a en b zijn gelijk.")
