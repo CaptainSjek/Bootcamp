4b91ecfb4c755f4011a403bd6584be263db4b3c2
CaptainSjek <99054715@mydavinci.nl>
2023-09-05 10:05:13 +0100

    Opdracht 4 afgemaakt

---
+++ Uitwerkingen/Les_5/uitwerkingen/Lest_5_opdracht_4.py
@@ -0,0 +1,9 @@
+#Schrijf code die de oppervlakte van een cirkel berekent, gebruik makend
+#van variabelen straal en pi = 3.14159. Voor het geval je het vergeten bent, de formule is
+#straal keer straal keer pi. 
+#Toon de uitkomst als volgt: “De oppervlakte van een cirkel met straal ... is ...”
+pi = 3.14159
+
+
+straal = int(input("vul hier de straal in "))
+print(straal * straal * pi)
