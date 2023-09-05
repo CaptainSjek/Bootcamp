90edfe503161f8c684003960e3676c27ff9b372f
CaptainSjek <99054715@mydavinci.nl>
2023-09-05 13:38:25 +0100

    Opdracht deels gemaakt met wat foutjes

---
+++ Uitwerkingen/Les_6/uitwerkingen/Les_6_opdracht_5.py
@@ -0,0 +1,25 @@
+# Opdracht 5 (hard):
+# Voer opdracht 2 nogmaals uit maar nu met 3 variabelen: a, b en c.
+# Hint: je kunt dit in twee stappen uitvoeren.
+# Vervolgens zorg je er ook weer voor dat de gebruiker de getallen kan invoeren.
+
+# a = int(input("Vul hier leeftijd A in."))
+# b = int(input("Vul hier leeftijd B in."))
+
+# if (a > b):
+#     print(f"variabele a is het grootst want {a} is groter dan {b}")
+# if (b > a):
+#     print(f"variabele b is het grootst want {b} is groter dan {a}")
+# if (a == b):
+#     print("variabele a en b zijn gelijk.")
+
+a = int(input("Vul hier leeftijd A in."))
+b = int(input("Vul hier leeftijd B in."))
+c = int(input("Vul hier leeftijd C in."))
+
+if (a > b):
+    print(f"variabele a is het grootst want {a} is groter dan {b}")
+else:
+print(f"")
+    print(f"variabele b is het grootst want")    
+
