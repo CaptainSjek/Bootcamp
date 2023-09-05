60432bc2ff344b3c5ad4dcbc2e6e7ef4522b4d3d
CaptainSjek <99054715@mydavinci.nl>
2023-09-05 12:47:53 +0100

    opdracht 3 opgelost

---
+++ Uitwerkingen/Les_6/uitwerkingen/Les_6_opdracht_3.py
@@ -0,0 +1,15 @@
+#Opdracht 3:
+#Schrijf een programma met één variabele: leeftijd.
+#Als de waarde van de variabele 16 of hoger is print je: 
+#"Gefeliciteerd, je mag je brommerrijbewijs halen." 
+#Anders print je:
+#"Helaas, je zult nog even moeten wachten."
+#
+#Test je programma door alleen de variabele leeftijd een andere waarde te geven. 
+#Test ook eens met 16.
+
+leeftijd = int(input("vul hier je leeftijd in"))
+
+if leeftijd >= 16:
+    print("Gefeliciteerd, je mag je brommerrijbewijs halen.")
+else:("Helaas, je zult nog even moeten wachten")
