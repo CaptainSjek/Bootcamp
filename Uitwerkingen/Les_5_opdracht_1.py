e68a601d8e6f4c55dcbda73854975e755a519f39
CaptainSjek <99054715@mydavinci.nl>
2023-09-05 08:24:01 +0100

    rest van de opdrachten toegevoegd aan repository

---
+++ Uitwerkingen/Les_5_opdracht_1.py
@@ -0,0 +1,5 @@
+naam= str(("252"))
+tekst = "Hallo " + naam +", ik leer nu programmeren."
+print(tekst)
+
+print("Vraag uit opdracht: Welke Operator(s) heb je gebruikt? Antwoord: ik heb '=' gebruikt en de '+' ")
