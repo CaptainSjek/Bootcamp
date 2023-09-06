9cd3aac799465bba43108efd65d17c240a2f031f
CaptainSjek <99054715@mydavinci.nl>
2023-09-06 09:44:59 +0100

    Opdracht 2 van les 7 afgemaakt

---
+++ Uitwerkingen/Les_7/uitwerkingen/Les_7_opdracht_2.py
@@ -0,0 +1,9 @@
+ # Opdracht 2:
+# Print 20 keer:
+# "Ik ben hard op weg developer te worden!"
+# (Let op: je mag maar 4 regels code gebruiken)
+
+i = 1
+while i < 21:
+    print("Hard op weg developer te worden!")
+    i += 1 
