f5a146402db3468089c598bb61970fc5a9a83789
CaptainSjek <99054715@mydavinci.nl>
2023-09-06 13:21:15 +0100

    Opdracht 1 en 2 van Les 8 gemaakt

---
+++ Uitwerkingen/Les_8/uitwerkingen/Les_8_opdracht_1.py
@@ -0,0 +1,47 @@
+# Opdracht 1:
+# In Nederland krijg je op school regelmatig een cijfer.
+# Wist je dat bij die cijfers ook een omschrijving hoort? 
+# Schrijf jij een programma wat de juiste omschrijving print bij een cijfer.
+
+# Houd rekening met het volgende:
+# Je schrijft een programma met 1 variabele: cijfer. Dit cijfer gebruik je om de omschrijving te printen:
+
+# Cijfer	Omschrijving
+# 10	uitmuntend
+# 9	zeer goed
+# 8	goed
+# 7	ruim voldoende
+# 6	voldoende
+# 5	bijna voldoende
+# 4	onvoldoende
+# 3	gering
+# 2	slecht
+# 1	zeer slecht
+
+cijfer = int(input("Type hier je cijfer "))
+
+
+if cijfer == 1:
+    print("Zeer slecht")
+elif cijfer == 2:
+    print("Slecht")
+elif cijfer == 3:
+    print("Gering")
+elif cijfer == 4:
+    print("Onvoldoende")
+elif cijfer == 5:
+    print("Bijna Voldoende")
+elif cijfer == 6:
+    print("Voldoende")
+elif cijfer == 7:
+    print("Ruim voldoende")
+elif cijfer == 8:
+    print("Goed")
+elif cijfer == 9:
+    print("Zeer goed")
+elif cijfer == 10:
+    print("Uitmuntend")
+elif cijfer == 0:
+    print("Sorry maar het is niet mogelijk om een 0 als cijfer te scoren")
+else:
+    print("Sorry maar het is niet mogelijk om een cijfer hoger dan 10 te scoren")
---
+++ Uitwerkingen/Les_8/uitwerkingen/Les_8_opdracht_2.py
@@ -0,0 +1,39 @@
+# Opdracht 2:
+# Oh ja: we (oefenen het) formatteren het ook nog even netjes:
+# Is je cijfer 6 of groter dan print je:
+# "Gefeliciteerd, {omschrijving} je resultaat is een {cijfer}"
+
+# Heb je lager dan een 6 gescoord dan wordt het: 
+# "Jammer, {omschrijving} je resultaat is een {cijfer}"
+
+# Zorg ervoor dat een gebruiker een cijfer tussen 1 en 10 kan invoeren. 
+# Wordt het getal te groot of te klein dan moet je een melding geven: 'Dit kan ik niet omzetten!'
+
+cijfer = int(input("Type hier je cijfer "))
+
+if cijfer == 1:
+    omschrijving = "Zeer slecht"
+elif cijfer == 2:
+    omschrijving = "Slecht"
+elif cijfer == 3:
+    omschrijving = "Gering"
+elif cijfer == 4:
+    omschrijving = "Onvoldoende"
+elif cijfer == 5:
+    omschrijving = "Bijna Voldoende"
+elif cijfer < 6:
+    omschrijving = "Voldoende"
+elif cijfer == 7:
+    omschrijving = "Ruim voldoende"
+elif cijfer == 8:
+    omschrijving = "Goed"
+elif cijfer == 9:
+    omschrijving = "Zeer goed"
+elif cijfer == 10:
+    omschrijving = "Uitmuntend"
+else:
+    print("Sorry maar het is niet mogelijk om een cijfer hoger dan 10 te scoren")
+if cijfer >= 6:
+    print(f"Gefeliciteerd, {omschrijving} je resultaat is een {cijfer}")
+if cijfer < 6:
+    print(f"Jammer, {omschrijving} je resultaat is een {cijfer}")
