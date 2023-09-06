3730bf04decad10350954576ff9d81f8c683898c
CaptainSjek <99054715@mydavinci.nl>
2023-09-06 09:33:48 +0100

    Opdracht afgemaakt

--- Uitwerkingen/Les_6/uitwerkingen/Les_6_opdracht_5.py
+++ Uitwerkingen/Les_6/uitwerkingen/Les_6_opdracht_5.py
@@ -13,13 +13,17 @@
 # if (a == b):
 #     print("variabele a en b zijn gelijk.")
 
-a = int(input("Vul hier leeftijd A in."))
-b = int(input("Vul hier leeftijd B in."))
-c = int(input("Vul hier leeftijd C in."))
+a = int(input("Vul hier leeftijd A in. "))
+b = int(input("Vul hier leeftijd B in. "))
+c = int(input("Vul hier leeftijd C in. "))
 
-if (a > b):
-    print(f"variabele a is het grootst want {a} is groter dan {b}")
+if a > b and a > c:
+    print(f"variabele A is het grootst want {a} is groter dan {b} en groter dan {c}")
+elif b > a and b > c:
+    print(f"variabele B is het grootst want {b} is groter dan {a} en groter dan {a}")
+elif c > a and c > b:
+    print(f"variabele C is het grootst want {c} is groter dan {a} en groter dan {b}")
 else:
-print(f"")
-    print(f"variabele b is het grootst want")    
+    print(f"Variabele A, B en C zijn allemaal gelijk") 
+   
 
