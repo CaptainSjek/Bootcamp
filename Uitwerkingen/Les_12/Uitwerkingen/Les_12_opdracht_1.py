d5919185ddf775d72b0228f81c5bf25d602f1585
CaptainSjek <99054715@mydavinci.nl>
2023-09-08 11:59:10 +0100

    Opdracht 1 en 2 van les 12 afgemaakt

--- Uitwerkingen/Les_12/Uitwerkingen/Les_12_opdracht_1.py
+++ Uitwerkingen/Les_12/Uitwerkingen/Les_12_opdracht_1.py
@@ -3,5 +3,6 @@
 # Gebruik vervolgens typecasting om de leeftijd van de gebruiker om te zetten in een integer en bereken hoe oud ze zullen zijn over 5 jaar.
 
 Leeftijd = int(input("Vul hier je leeftijd in. "))
+Naam = input("Vul hier je naam in. ")
 jaar = 5
-print("Over 5 jaar ben je" , Leeftijd + jaar , "oud.")
+print(f"Hoi {Naam}, over 5 jaar ben je {Leeftijd + jaar} oud.")
---
+++ Uitwerkingen/Les_12/Uitwerkingen/Les_12_opdracht_2.py
@@ -0,0 +1,8 @@
+# Opdracht 2:
+# Schrijf een programma dat de gebruiker vraagt om twee getallen in te voeren. 
+# Gebruik vervolgens typecasting om de getallen om te zetten in floats en bereken het gemiddelde van de twee getallen.
+
+getal1= float(input("Vul hier een willekeurig getal in. "))
+getal2 = float(input("Vul hier een willekeurig getal in. "))
+
+print(f"Het gemiddelde van de 2 getallen is: {(getal1 + getal2) / 2}")
