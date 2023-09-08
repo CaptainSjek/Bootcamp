5d0db96f8c2c0394c639aeab3cab8b13018f80a9
CaptainSjek <99054715@mydavinci.nl>
2023-09-08 11:39:05 +0100

    Bugs opgelost opdracht afgemaakt

--- Uitwerkingen/Les_7/uitwerkingen/Demo.py
+++ Uitwerkingen/Les_7/uitwerkingen/Demo.py
@@ -8,3 +8,43 @@
 
 
 # #..2f
+
+a = 5
+b = 9
+repeat = True
+aantal = 0
+
+# while repeat:
+#     print("ok")
+#     aantal += 1
+#     repeat = aantal < 5
+
+# repeat = True
+
+# while repeat:
+#     antwoord = input("Quit? ")
+#     repeat = antwoord !="y"
+
+
+# antwoord = input("Quit? ")
+
+# if antwoord == "Ja" or antwoord== "J" or antwoord== "Y" or antwoord== "Yes" or antwoord== "ja" or antwoord== "j" or antwoord== "ja" or antwoord== "y" or antwoord== "yes":
+#     print("OK ")
+# else:
+#     print("NOK")
+
+antwoord1 = input("Quit ") #Ja J
+antwoord2 = input("getal ") #meer dan 5
+
+if antwoord1 == "Ja" or antwoord1 == "J" and antwoord2 > 5:
+    print("OK")
+else:
+    print("NOK")
+
+
+
+
+
+#while b>a:
+#    print("True")
+#    b-=1
