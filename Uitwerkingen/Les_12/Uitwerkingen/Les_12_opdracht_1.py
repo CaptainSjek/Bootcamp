86617a0b843d1c96b795bc61391e7defb5232fa3
CaptainSjek <99054715@mydavinci.nl>
2023-09-08 11:46:58 +0100

    Opdracht 1 van les 12 afgemaakt

---
+++ Uitwerkingen/Les_12/Uitwerkingen/Les_12_opdracht_1.py
@@ -0,0 +1,7 @@
+# Opdracht 1:
+# Schrijf een programma dat de gebruiker vraagt om hun naam en leeftijd in te voeren. 
+# Gebruik vervolgens typecasting om de leeftijd van de gebruiker om te zetten in een integer en bereken hoe oud ze zullen zijn over 5 jaar.
+
+Leeftijd = int(input("Vul hier je leeftijd in. "))
+jaar = 5
+print("Over 5 jaar ben je" , Leeftijd + jaar , "oud.")
