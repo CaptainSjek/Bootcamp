b2fd15b8ce8fabcddb7ced82c94bef50c292c0a6
CaptainSjek <99054715@mydavinci.nl>
2023-09-11 13:56:28 +0100

    Les_15_opdracht_1 afgemaakt

---
+++ Uitwerkingen/Les_15/Uitwerkingen/Les_15_opdracht_1.py
@@ -0,0 +1,12 @@
+# Opdracht 1:
+# Schrijf een functie met twee parameters (integers).
+# De functie retourneert de som van de twee parameters.
+get1 = 25
+get2 = 10
+
+
+def toevoegen(c, h):
+    i = c + h
+    return i
+print(toevoegen(get1 , get2 ))
+    
