c29fe19a7f54844d8698f7aaefb8a0987c1fd1dd
CaptainSjek <99054715@mydavinci.nl>
2023-09-11 10:03:11 +0100

    opdracht 1 van les 14 gemaakt

---
+++ Uitwerkingen/Les_14/Uitwerkingen/Les_14_opdracht_1.py
@@ -0,0 +1,7 @@
+# Opdracht 1:
+# Schrijf een programma dat een lege lijst maakt en vervolgens de gebruiker vraagt om 5 getallen in te voeren. 
+# Gebruik de append functie om elk getal aan de lijst toe te voegen en print vervolgens de lijst.
+
+cijfers = []
+cijfers.append(input("Vul hier een getal in dat je wilt toevoegen aan de lijst. "))
+print(cijfers)
