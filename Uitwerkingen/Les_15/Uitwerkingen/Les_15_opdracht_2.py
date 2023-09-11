98bc8c44203fb462bccf1ede61842c35ab3fbbf7
CaptainSjek <99054715@mydavinci.nl>
2023-09-11 14:00:44 +0100

    Opdracht 2 van les 15 afgemaakt

---
+++ Uitwerkingen/Les_15/Uitwerkingen/Les_15_opdracht_2.py
@@ -0,0 +1,11 @@
+# Opdracht 2:
+# Schrijf een functie met twee parameters (integers).
+# De functie retourneert de vermenigvuldiging van de twee parameters.
+
+get1 = 20
+get2 = 10
+
+def vermenigvuldiging(a , b):
+    c = a * b
+    return c
+print(vermenigvuldiging(get1, get2))
