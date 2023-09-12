cd1802826b617a28499e82f57384140039b404b0
CaptainSjek <99054715@mydavinci.nl>
2023-09-12 13:08:15 +0100

    Opdracht 4 afgemaakt

--- Uitwerkingen/Les_10_Toets/Uitwerkingen/Les_10_opdracht_4.py
+++ Uitwerkingen/Les_10_Toets/Uitwerkingen/Les_10_opdracht_4.py
@@ -9,10 +9,12 @@ prijs_m2 = 40
 
 if oppervlakte >= 150:
     prijs_m2 = 35
-if oppervlakte >= 80:
+    totaal = prijs_m2 * oppervlakte
+
+elif oppervlakte >= 80:
     prijs_m2 = 38
 
-totaal = prijs_m2 * oppervlakte
+    totaal = prijs_m2 * oppervlakte
 
 print(f'Het totaalbedrag is voor een oppervlakte van {oppervlakte} m2 is: Eur ' + str(totaal))
 
