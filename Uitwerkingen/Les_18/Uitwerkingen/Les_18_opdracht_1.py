df575ddc0f940a98fb1815ada28cc89cdb9bb61f
CaptainSjek <99054715@mydavinci.nl>
2023-09-14 09:49:43 +0100

    Les 17 verbeterd en les 18 afgemaakt

--- Uitwerkingen/Les_18/Uitwerkingen/Les_18_opdracht_1.py
+++ Uitwerkingen/Les_18/Uitwerkingen/Les_18_opdracht_1.py
@@ -1,8 +1,10 @@
-# Opdracht 4:
-# Verslavend: de gebruikers vinden je game zo leuk dat ze er niet mee kunnen stoppen.
-# Pas je game daarom als volgt aan: 
-# - goed geraden? Vraag of de gebruiker nog een ronde wil spelen.
-# - aan het einde print je het aantal gespeelde ronden en het aantal keer dat de gebruiker fout heeft geraden.
+# Opdracht 1:
+# je spel (van de vorige les) wordt een hit.
+# Gebruikers klagen alleen dat het erg lang duurt voordat ze klaar zijn met raden.
+# Na diepgravend onderzoek kom er achter dat je nergens meldt in welke range men kan raden en gebruikers blijken dus te denken dat ze tussen 1 en 100 mogen raden.
+
+# Breid je gereedschap uit met een functie die kan controleren of de ingevoerde waarde in een lijst voorkomt. 
+# Je kunt deze functie op twee plaatsen inzetten. Waar? En doe dit dan ook.
 
 import random               #Zorgt ervoor dat getallen door de computer gerandomized kunnen worden
 from Raadspel import *      #Zorgt ervoor dat de functie gebruikt kan worden
@@ -17,18 +19,15 @@ antwoord = "j, ja, y, yes"  #Input of de gebruiker door wilt spelen
 
 print(getal)
 while doorgaan:                                                                 #
-    if inputgetal > 5:
-        raise ValueError
-        break
-
-
     try:                                                                        # Hier vraagt de computer de speler om een getal tussen de 1 en 5 te kiezen
         inputgetal = int(input("Vul hier een getal in tussen de 1 en 5: "))     # Als de gebruiker geen 
-
+        if inputgetal > 5:
+            raise ValueError
+        
     except ValueError:                                                          #
-        print("Dit getal is hoger dan 5! Je verliest een leven :-( ")           #
+        print("\033[31mDit getal is hoger dan 5! Dit kost je een leven :-(\033[0m ")           #
     if inputgetal == getal:                                                     #
-        print(f"\033[32mJe hebt het getal goed geraden!\033[0m")                #
+        print(f"\033[32mJe hebt het getal goed geraden! \033[0m")                #
         vraag = input("Wil je nog een keer spelen? Type J of N: ").lower()      #
         rondes += 1                                                             #
         if vraag in antwoord:                                                   #
