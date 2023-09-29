963cdc0f2a967965d54ff520e9ffc542ddde755f
CaptainSjek <99054715@mydavinci.nl>
2023-09-29 10:39:41 +0100

    Opdracht 1 aangemaakt

---
+++ Uitwerkingen/Toets/Toets_bootcamp_opdracht.py
@@ -0,0 +1,6 @@
+# Opdracht 1 (schrijf je antwoord als commentaar in een py file):
+
+
+# a: Waarom is Visual Studio Code handiger voor software development dan bijvoorbeeld Notepad (kladblok)? Noem de voordelen!
+
+# b: Waarom is het goed dat je de commits van jouw code pusht naar github.com?
